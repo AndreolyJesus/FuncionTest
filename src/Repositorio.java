d2b973c70778c48e25b553c5f0f6454a38a39013
Andreoly Jesus <andreolyjesus@hotmail.com>
2022-01-22 11:52:20 -0300

    test inicial

---
+++ src/Repositorio.java
@@ -0,0 +1,21 @@
+
+
+public class Repositorio {
+
+    public static void main(String[] args) {
+    //int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+        double salarioDoblo = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 10;
+
+        System.out.println("A idade é "+idade+"anos");
+        System.out.println("falso");
+        System.out.println("char"+caractere);
+
+        }
+}
